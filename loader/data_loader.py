da8a3824d0bcc7ed8e8b06e833f698e0695630d3
Joel Tay <joeltwh1999@gmail.com>
2024-04-01 14:02:08 +0800

    add opensmile feature extraction and add to data loader

--- loader/data_loader.py
+++ loader/data_loader.py
@@ -1,11 +1,12 @@
 import os
 import torch
 import torch.utils.data as data
-import pandas as pd
 import numpy as np
 from PIL import Image
 import json
 from scipy.interpolate import interp1d
+import pandas as pd
+
 
 class PMEmoDataset(data.Dataset):
     def __init__(self, root_dir):
@@ -14,13 +15,13 @@ class PMEmoDataset(data.Dataset):
         self.eda_dir = os.path.join(root_dir, "eda")
         self.spectrograms_dir = os.path.join(root_dir, "spectrograms")
         self.music_ids = self._get_music_ids()
-        
+
     def __len__(self):
         return len(self.music_ids)
-    
+
     def __getitem__(self, index):
         music_id = self.music_ids[index]
-        
+
         # Load annotations
         arousal_file = os.path.join(self.annotations_dir, "Arousal", f"{music_id}-A.csv")
         valence_file = os.path.join(self.annotations_dir, "Valence", f"{music_id}-V.csv")
@@ -31,17 +32,17 @@ class PMEmoDataset(data.Dataset):
 
         with open(arousal_file, 'r') as file:
             next(file)
-            for line in file: 
+            for line in file:
                 values = line.split(',')
                 subject_ids.append(values[0])
                 arousal_labels.append(float(values[1]))
-        
+
         with open(valence_file, 'r') as file:
             next(file)
-            for line in file: 
+            for line in file:
                 values = line.split(',')
                 valence_labels.append(float(values[1]))
-                
+
         # If there are more than 10 subjects, only take the first 10
         if len(subject_ids) > 10:
             subject_ids = subject_ids[:10]
@@ -55,7 +56,7 @@ class PMEmoDataset(data.Dataset):
             with open(eda_file, "r") as f:
                 eda_signal = np.array(json.loads(f.read()))
             eda_data.append(eda_signal)
-        
+
         # Interpolate the EDA sequences to have a length of 896
         interpolated_eda_data = []
         for eda_signal in eda_data:
@@ -69,7 +70,7 @@ class PMEmoDataset(data.Dataset):
             interpolated_eda_data.append(interpolated_signal)
 
         eda_data = np.array(interpolated_eda_data)
-        
+
         # Load spectrogram
         spectrogram_file = os.path.join(self.spectrograms_dir, f"{music_id}.png")
         spectrogram = Image.open(spectrogram_file)
@@ -78,13 +79,17 @@ class PMEmoDataset(data.Dataset):
 
         eda_data = torch.tensor(eda_data, dtype=torch.float32)
         spectrogram = torch.tensor(spectrogram, dtype=torch.float32).unsqueeze(0)
-        
 
         arousal_labels = torch.tensor(arousal_labels, dtype=torch.float32)
         valence_labels = torch.tensor(valence_labels, dtype=torch.float32)
-        
-        return spectrogram, eda_data, arousal_labels, valence_labels
-    
+
+        # opensmile music features
+        music_df = pd.read_csv("/Users/joel-tay/Desktop/multi_modal_cnn/dataset/static_features.csv",
+                               index_col="musicId")
+        music_features = music_df.loc[music_df.index == 1000].iloc[0] # type pandas Series
+
+        return spectrogram, eda_data, arousal_labels, valence_labels, music_features
+
     def _get_music_ids(self):
         music_ids = set()
         for filename in os.listdir(os.path.join(self.annotations_dir, "Arousal")):
---
+++ preprocessing/wav_to_features.py
@@ -0,0 +1,39 @@
+import subprocess
+import pandas as pd
+import os
+import arff
+
+
+def get_music_features(wav_dir, dist_file, opensmile_dir):
+    # extract static features of all wavs and load into 1 file
+    SMILExtract = os.path.join(opensmile_dir, "build", "progsrc", "smilextract", "SMILExtract")
+    config_file = os.path.join(opensmile_dir, "config", "is09-13", "IS13_ComParE.conf")
+
+    if os.path.exists(dist_file):
+        os.remove(dist_file)
+
+    wav = [f for f in os.listdir(wav_dir) if f[-4:] == ".wav"]
+    for i, w in enumerate(wav):
+        wavpath = os.path.join(wav_dir, w)
+        subprocess.check_call([SMILExtract, "-C", config_file, "-I", wavpath, "-O", dist_file, "-instname", w])
+        print(f"completed {i+1}/{len(wav)} wav files")
+
+
+def wav_to_features():
+    static_features_file = "static_features.arff"
+    get_music_features("/Users/joel-tay/Desktop/multi_modal_cnn/dataset/wav",
+                       static_features_file,
+                       "/Users/joel-tay/Documents/opensmile")
+    res = arff.load(open(static_features_file, "r"))
+    data = res['data']
+    cols = list(map(lambda t: t[0], res['attributes']))
+    df = pd.DataFrame(data, columns=cols)
+    # exclude last col "class", not relevant from opensmile
+    df = df.drop(columns=['class'])
+    df['name'] = df['name'].map(lambda s: int(s[:-4]))  # remove .wav and cast to int
+    df.rename(columns={'name': 'musicId'}, inplace=True)
+    df.to_csv("/Users/joel-tay/Desktop/multi_modal_cnn/dataset/static_features.csv", index=False)
+
+
+if __name__ == "__main__":
+    wav_to_features()
