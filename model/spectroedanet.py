282bd1eb470443edf989e48bcf08bfb7bd397186
Chang Su <suchang011124@gmail.com>
2024-04-15 21:55:16 +0800

    attention update

--- model/spectroedanet.py
+++ model/spectroedanet.py
@@ -1,10 +1,17 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from model.Attention_module import NONLocal1D, CALayer1D, NONLocal2D, CALayer2D
 
 
 class SpectroEDANet(nn.Module):
-    def __init__(self, usesSpectrogram=True, usesEDA=True, usesMusic=True, usesAttention = True, predictsArousal=True, predictsValence=True):
+    def __init__(self,
+                 usesSpectrogram=True,
+                 usesEDA=True,
+                 usesMusic=True,
+                 usesAttention = True,
+                 predictsArousal=True,
+                 predictsValence=True):
         super(SpectroEDANet, self).__init__()
         self.usesSpectrogram = usesSpectrogram
         self.usesEDA = usesEDA
@@ -39,7 +46,6 @@ class SpectroEDANet(nn.Module):
             nn.AdaptiveAvgPool1d(1),
             nn.Flatten()
         )
-        
 
         self.inter_channel = 128
         # Attention Mechanism
@@ -47,15 +53,7 @@ class SpectroEDANet(nn.Module):
         self.Attention1D = CALayer1D(channel=self.inter_channel)
         self.Non_local2D = NONLocal2D(in_feat=self.inter_channel, inter_feat=self.inter_channel // 2)
         self.Attention2D = CALayer2D(channel=self.inter_channel)
-        
-        self.music_cnn = nn.Sequential(
-            nn.Linear(6373, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 128)
-        )
-        
+
         if self.usesAttention:
             # Spectrogram CNN
             self.spec_cnn = nn.Sequential(
@@ -84,18 +82,14 @@ class SpectroEDANet(nn.Module):
                 #            nn.AdaptiveAvgPool1d(1),
                 #            nn.Flatten()
                 nn.BatchNorm1d(128)
-            )       
-            
-#             # Music CNN
-#             self.music_cnn = nn.Sequential(
-#                 nn.Linear(6373, 1024),
-#                 nn.ReLU(),
-#                 nn.Linear(1024, 512),
-#                 nn.ReLU(),
-#                 nn.Linear(512, 128),
-#                 nn.ReLU(),
-#                 nn.BatchNorm1d(128)
-#             )       
+            )
+
+        music_features = 319
+        lstm_hidden_size = 200
+        # LSTM model inspired by this paper: https://www.sciencedirect.com/science/article/pii/S2215098620342385
+        self.music_lstm = nn.LSTM(music_features, lstm_hidden_size, batch_first=True)
+        self.music_fc1 = nn.Linear(lstm_hidden_size, 128)
+        self.music_fc2 = nn.Linear(128, 128)
 
         # Fusion layer
         fusion_input_size = 0
@@ -111,21 +105,27 @@ class SpectroEDANet(nn.Module):
         self.fusion = nn.Linear(fusion_input_size, 256)
 
         # Multi-Task Output layers
-        # self.arousal_output = nn.Linear(fusion_input_size, 10)
-        # self.valence_output = nn.Linear(fusion_input_size, 10)
-        self.arousal_output = nn.Linear(256, 10)
-        self.valence_output = nn.Linear(256, 10)
+        # self.arousal_output = nn.Linear(fusion_input_size, 1)
+        # self.valence_output = nn.Linear(fusion_input_size, 1)
+        self.arousal_output = nn.Linear(256, 1)
+        self.valence_output = nn.Linear(256, 1)
 
         # Single-Task Output layer
         self.output = nn.Sequential(
             # nn.Linear(fusion_input_size, 128),
             nn.Linear(256, 128),
             nn.ReLU(inplace=True),
-            nn.Linear(128, 10),
+            nn.Linear(128, 1),
             nn.ReLU(inplace=True)
         )
 
+    def __str__(self):
+        return f'Model with params: usesSpectrogram = {self.usesSpectrogram}, usesEDA = {self.usesEDA}, usesMusic = {self.usesMusic}, usesAttention = {self.usesAttention}'
+
     def forward(self, spectrogram, eda_data, music_vector):
+        print(spectrogram.shape)
+        print(eda_data.shape)
+        print(music_vector.shape)
         # Initialize an empty tensor to store the fused features
         fused_features = []
 
@@ -147,19 +147,20 @@ class SpectroEDANet(nn.Module):
                 eda_features = self.Non_local1D(eda_features)
                 eda_features = self.Attention1D(eda_features)
 
+
         if self.usesMusic:
-            music_features = self.music_cnn(music_vector)
+            music_features = music_vector.unsqueeze(1)
+            lstm_out, _ = self.music_lstm(music_features)
+            music_features = lstm_out[:, -1, :]
+            music_features = F.relu(self.music_fc1(music_features))
+            music_features = F.relu(self.music_fc2(music_features))
             fused_features.append(music_features)
-#             # Using the attention mechanism
-#             if self.usesAttention:
-#                 music_features = self.Non_local1D(music_features)
-#                 music_features = self.Attention1D(music_features)        
-            
+
         # Fusion of spectrogram and EDA features
         if self.usesAttention != True:
             fused_features = torch.cat(tuple(fused_features), dim=1)
             fused_features = self.fusion(fused_features)
-        
+
         if self.usesAttention:
             eda_features_reshaped = eda_features.unsqueeze(2).expand(-1, -1, 92, -1)
             fused_features = torch.cat((spec_features, eda_features_reshaped), dim=3)
@@ -173,8 +174,8 @@ class SpectroEDANet(nn.Module):
             
             self.fusion = nn.Linear(256, 256)
             fused_features = self.fusion(fused_features)
-            
-        # Output layers    
+
+        # Output layers
         if self.predictsArousal and self.predictsValence:
             arousal_output = self.arousal_output(fused_features)
             valence_output = self.valence_output(fused_features)
