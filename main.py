a5049366f4a98f580cc04a00297bc585db92c82d
Joel Tay <joeltwh1999@gmail.com>
2024-04-16 02:45:57 +0800

    change code for faster experiments

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from sklearn.metrics import root_mean_squared_error, r2_score
 from copy import deepcopy
 from datetime import datetime
 
-root_dir = "dataset"
-dataset = PMEmoDataset(root_dir)
 
+# root_dir = "dataset"
+# dataset = PMEmoDataset(root_dir)
 
 def unpack_data(data, device: torch.device):
     spectrogram, eda_data, arousal_label, valence_label, music_vector = data
@@ -28,11 +28,12 @@ def unpack_data(data, device: torch.device):
 def main(usesSpectrogram=True,
          usesEDA=False,
          usesMusic=False,
-         usesAttention = True,
+         usesAttention=True,
          predictsArousal=True,
-         predictsValence=True) -> (
+         predictsValence=True,
+         dataset=None) -> (
         tuple[
-            SpectroEdaMusicNet,
+            spectroedanet.SpectroEDANet,
             dict[str, Any],
             list[float],
             list[float],
@@ -49,6 +50,10 @@ def main(usesSpectrogram=True,
     if (not is_predicting) or (not is_using_features):
         # if not predicting or not using any features, pass
         return
+    if usesAttention:
+        if not (usesSpectrogram and usesEDA and usesMusic):
+            # if we are doing attention, make sure everything is in use
+            return
 
     model = spectroedanet.SpectroEDANet(usesSpectrogram,
                                         usesEDA,
@@ -56,24 +61,24 @@ def main(usesSpectrogram=True,
                                         usesAttention,
                                         predictsArousal,
                                         predictsValence)
-    
+
     # Split the dataset into training and testing sets
     train_val_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
 
     # Define loss function and optimizer
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
     # Training loop
-    num_epochs = 2
+    num_epochs = 10
     device = torch.device(
         "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
     # Define the number of folds for cross-validation
-    num_folds = 2
+    num_folds = 5
 
-    batch_size = 2
+    batch_size = 32
 
     # Create a KFold object
     kfold = KFold(n_splits=num_folds, shuffle=True)
@@ -85,7 +90,7 @@ def main(usesSpectrogram=True,
     best_valence_r2: float | None = None
     best_arousal_rmse: float | None = None
     best_valence_rmse: float | None = None
-    trials = 0 # in this case trials is controlled model-level, not within each k-fold
+    trials = 0  # in this case trials is controlled model-level, not within each k-fold
     patience = 15
     best_found = False
     train_losses = {i: [] for i in range(num_folds)}
@@ -117,18 +122,14 @@ def main(usesSpectrogram=True,
 
                 output = model(spectrogram, eda_data, music_vector)
 
-                # if model.predictsArousal and model.predictsValence:
-                #     arousal_loss = criterion(output[0], arousal_label)
-                #     valence_loss = criterion(output[1], valence_label)
-                #     loss = arousal_loss + valence_loss
-                # elif model.predictsArousal:
-                #     loss = criterion(output, arousal_label)
-                # elif model.predictsValence:
-                #     loss = criterion(output, valence_label)
-
-                arousal_loss = criterion(output[0], arousal_label)
-                valence_loss = criterion(output[1], valence_label)
-                loss = arousal_loss + valence_loss
+                if model.predictsArousal and model.predictsValence:
+                    arousal_loss = criterion(output[0], arousal_label)
+                    valence_loss = criterion(output[1], valence_label)
+                    loss = arousal_loss + valence_loss
+                elif model.predictsArousal:
+                    loss = criterion(output, arousal_label)
+                elif model.predictsValence:
+                    loss = criterion(output, valence_label)
 
                 loss.backward()
                 optimizer.step()
@@ -147,99 +148,74 @@ def main(usesSpectrogram=True,
                 for data in val_loader:
                     spectrogram, eda_data, arousal_label, valence_label, music_vector = unpack_data(data, device)
 
-                    arousal_output, valence_output = model(spectrogram, eda_data, music_vector)
-                    arousal_loss = criterion(arousal_output, arousal_label)
-                    valence_loss = criterion(valence_output, valence_label)
-                    val_loss += arousal_loss.item() + valence_loss.item()
-                    arousal_preds.extend(arousal_output.cpu().numpy())
-                    valence_preds.extend(valence_output.cpu().numpy())
-                    arousal_labels.extend(arousal_label.cpu().numpy())
-                    valence_labels.extend(valence_label.cpu().numpy())
+                    if model.predictsArousal and model.predictsValence:
+                        arousal_output, valence_output = model(spectrogram, eda_data, music_vector)
+                        arousal_loss = criterion(arousal_output, arousal_label)
+                        valence_loss = criterion(valence_output, valence_label)
+                        val_loss += arousal_loss.item() + valence_loss.item()
+                        arousal_preds.extend(arousal_output.cpu().numpy())
+                        valence_preds.extend(valence_output.cpu().numpy())
+                        arousal_labels.extend(arousal_label.cpu().numpy())
+                        valence_labels.extend(valence_label.cpu().numpy())
 
-                    # if model.predictsArousal and model.predictsValence:
-                    #     arousal_output, valence_output = model(spectrogram, eda_data, music_vector)
-                    #     arousal_loss = criterion(arousal_output, arousal_label)
-                    #     valence_loss = criterion(valence_output, valence_label)
-                    #     val_loss += arousal_loss.item() + valence_loss.item()
-                    #     arousal_preds.extend(arousal_output.cpu().numpy())
-                    #     valence_preds.extend(valence_output.cpu().numpy())
-                    #     arousal_labels.extend(arousal_label.cpu().numpy())
-                    #     valence_labels.extend(valence_label.cpu().numpy())
-                    #
-                    # elif model.predictsArousal:
-                    #     output = model(spectrogram, eda_data, music_vector)
-                    #     val_loss = criterion(output, arousal_label)
-                    #     arousal_preds.extend(output.cpu().numpy())
-                    #     arousal_labels.extend(arousal_label.cpu().numpy())
-                    #
-                    # elif model.predictsValence:
-                    #     output = model(spectrogram, eda_data, music_vector)
-                    #     val_loss = criterion(output, valence_label)
-                    #     valence_preds.extend(output.cpu().numpy())
-                    #     valence_labels.extend(valence_label.cpu().numpy())
+                    elif model.predictsArousal:
+                        output = model(spectrogram, eda_data, music_vector)
+                        val_loss = criterion(output, arousal_label)
+                        arousal_preds.extend(output.cpu().numpy())
+                        arousal_labels.extend(arousal_label.cpu().numpy())
+
+                    elif model.predictsValence:
+                        output = model(spectrogram, eda_data, music_vector)
+                        val_loss = criterion(output, valence_label)
+                        valence_preds.extend(output.cpu().numpy())
+                        valence_labels.extend(valence_label.cpu().numpy())
 
                 arousal_r2 = None
                 valence_r2 = None
                 # Calculate and print losses and metrics
-                arousal_rmse = root_mean_squared_error(arousal_labels, arousal_preds)
-                valence_rmse = root_mean_squared_error(valence_labels, valence_preds)
-                arousal_r2 = r2_score(arousal_labels, arousal_preds)
-                valence_r2 = r2_score(valence_labels, valence_preds)
+                if model.predictsArousal and model.predictsValence:
+                    arousal_rmse = root_mean_squared_error(arousal_labels, arousal_preds)
+                    valence_rmse = root_mean_squared_error(valence_labels, valence_preds)
+                    arousal_r2 = r2_score(arousal_labels, arousal_preds)
+                    valence_r2 = r2_score(valence_labels, valence_preds)
 
-                print(f"Epoch [{epoch + 1}/{num_epochs}], "
-                      f"Train Loss: {running_loss / len(train_loader):.4f}, "
-                      f"Val Loss: {val_loss / len(val_loader):.4f}, "
-                      f"Arousal RMSE: {arousal_rmse:.4f}, "
-                      f"Valence RMSE: {valence_rmse:.4f}, "
-                      f"Arousal R2: {arousal_r2:.4f}, "
-                      f"Valence R2: {valence_r2:.4f}")
-                # if model.predictsArousal and model.predictsValence:
-                #     arousal_rmse = root_mean_squared_error(arousal_labels, arousal_preds)
-                #     valence_rmse = root_mean_squared_error(valence_labels, valence_preds)
-                #     arousal_r2 = r2_score(arousal_labels, arousal_preds)
-                #     valence_r2 = r2_score(valence_labels, valence_preds)
-                #
-                #     print(f"Epoch [{epoch + 1}/{num_epochs}], "
-                #           f"Train Loss: {running_loss / len(train_loader):.4f}, "
-                #           f"Val Loss: {val_loss / len(val_loader):.4f}, "
-                #           f"Arousal RMSE: {arousal_rmse:.4f}, "
-                #           f"Valence RMSE: {valence_rmse:.4f}, "
-                #           f"Arousal R2: {arousal_r2:.4f}, "
-                #           f"Valence R2: {valence_r2:.4f}")
-                #
-                # elif model.predictsArousal:
-                #     arousal_rmse = root_mean_squared_error(arousal_labels, arousal_preds)
-                #     arousal_r2 = r2_score(arousal_labels, arousal_preds)
-                #     print(f"Epoch [{epoch + 1}/{num_epochs}], "
-                #           f"Train Loss: {running_loss / len(train_loader):.4f}, "
-                #           f"Val Loss: {val_loss / len(val_loader):.4f}, "
-                #           f"Arousal RMSE: {arousal_rmse:.4f}, "
-                #           f"Arousal R2: {arousal_r2:.4f}")
-                #
-                # elif model.predictsValence:
-                #     valence_rmse = root_mean_squared_error(valence_labels, valence_preds)
-                #     valence_r2 = r2_score(valence_labels, valence_preds)
-                #     print(f"Epoch [{epoch + 1}/{num_epochs}], "
-                #           f"Train Loss: {running_loss / len(train_loader):.4f}, "
-                #           f"Val Loss: {val_loss / len(val_loader):.4f}, "
-                #           f"Valence RMSE: {valence_rmse:.4f}, "
-                #           f"Valence R2: {valence_r2:.4f}")
+                    print(f"Epoch [{epoch + 1}/{num_epochs}], "
+                          f"Train Loss: {running_loss / len(train_loader):.4f}, "
+                          f"Val Loss: {val_loss / len(val_loader):.4f}, "
+                          f"Arousal RMSE: {arousal_rmse:.4f}, "
+                          f"Valence RMSE: {valence_rmse:.4f}, "
+                          f"Arousal R2: {arousal_r2:.4f}, "
+                          f"Valence R2: {valence_r2:.4f}")
+
+                elif model.predictsArousal:
+                    arousal_rmse = root_mean_squared_error(arousal_labels, arousal_preds)
+                    arousal_r2 = r2_score(arousal_labels, arousal_preds)
+                    print(f"Epoch [{epoch + 1}/{num_epochs}], "
+                          f"Train Loss: {running_loss / len(train_loader):.4f}, "
+                          f"Val Loss: {val_loss / len(val_loader):.4f}, "
+                          f"Arousal RMSE: {arousal_rmse:.4f}, "
+                          f"Arousal R2: {arousal_r2:.4f}")
+
+                elif model.predictsValence:
+                    valence_rmse = root_mean_squared_error(valence_labels, valence_preds)
+                    valence_r2 = r2_score(valence_labels, valence_preds)
+                    print(f"Epoch [{epoch + 1}/{num_epochs}], "
+                          f"Train Loss: {running_loss / len(train_loader):.4f}, "
+                          f"Val Loss: {val_loss / len(val_loader):.4f}, "
+                          f"Valence RMSE: {valence_rmse:.4f}, "
+                          f"Valence R2: {valence_r2:.4f}")
 
                 if val_loss < best_val_loss:
                     # NOTE: here best_val_loss is stored as val_loss without dividing by its length
                     # it should however not have any impact as it is just a means to detect better model weights
                     best_val_loss = val_loss
                     best_model_weights = deepcopy(model.state_dict())
-                    best_arousal_r2 = arousal_r2
-                    best_arousal_rmse = arousal_rmse
-                    est_valence_r2 = valence_r2
-                    best_valence_rmse = valence_rmse
-                    # if model.predictsArousal:
-                    #     best_arousal_r2 = arousal_r2
-                    #     best_arousal_rmse = arousal_rmse
-                    # if model.predictsValence:
-                    #     best_valence_r2 = valence_r2
-                    #     best_valence_rmse = valence_rmse
+                    if model.predictsArousal:
+                        best_arousal_r2 = arousal_r2
+                        best_arousal_rmse = arousal_rmse
+                    if model.predictsValence:
+                        best_valence_r2 = valence_r2
+                        best_valence_rmse = valence_rmse
                     trials = 0
                 else:
                     trials += 1
@@ -255,7 +231,7 @@ def main(usesSpectrogram=True,
     # plot average losses (epoch-average, across folds)
     avg_train_losses = [sum(epoch_losses) / len(epoch_losses) for epoch_losses in zip(*train_losses.values())]
     avg_val_losses = [sum(epoch_losses) / len(epoch_losses) for epoch_losses in zip(*val_losses.values())]
-    
+
     # Save model weights
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     # store the file in the checkpoints folder. the file name should use the boolean flags that are inputs to the main functions and end with the timestamp
@@ -266,11 +242,14 @@ def main(usesSpectrogram=True,
         filename += 'usesEDA_'
     if usesMusic:
         filename += 'usesMusic_'
+    if usesAttention:
+        filename += 'usesAttention_'
     if predictsArousal:
         filename += 'predictsArousal_'
     if predictsValence:
         filename += 'predictsValence_'
-    filename += f'{timestamp}.pt'
+    # filename += f'{timestamp}.pt'
+    filename += '.pt'
 
     print('Saving best model...')
     torch.save(model.state_dict(), filename)
